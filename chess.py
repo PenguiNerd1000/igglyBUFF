ae7caf0fc426f3fab4bb99bcd4e44cc94054442f
PenguiNerd1000 <107518929+PenguiNerd1000@users.noreply.github.com>
2023-03-28 10:54:12 -0700

    Update chess.py

--- chess.py
+++ chess.py
@@ -5,8 +5,19 @@ class Piece:
         self.value = value
         self.representation = representation
         
-    def moveInterpretation(pos1, pos2):
-        pass
+    def moveVector(pos1, pos2, pos3, pos4):
+        conversionLib = {
+        1: "a"
+        2: "b"
+        3: "c"
+        4: "d"
+        5: "e"
+        6: "f"
+        7: "g"
+        8: "h"
+}
+        vector = []
+        return vector
         
 class King(Piece): 
     promotion = False
